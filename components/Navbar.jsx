ab7669769217125cb8e584f1846f48ed887792c1
LAYI CODER <layiwola008@gmail.com>
2022-10-23 05:46:31 +0100

    Add mobile navigation

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,17 +1,23 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { BsTelephoneFill } from "react-icons/bs";
 import { BsArrowRightCircleFill } from "react-icons/bs";
-import {AiOutlineMenu} from 'react-icons/ai';
+import { AiOutlineMenu } from "react-icons/ai";
+import { GrClose } from "react-icons/gr";
 
 import Logo from "../public/images/asd-logo.png";
 import Link from "next/link";
 
 const Navbar = () => {
   const [page, setPage] = useState(1);
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
 
   function setCurrentPage(index) {
     setPage(index);
+  }
+
+  function setMenu() {
+    setShowMobileMenu(!showMobileMenu);
   }
 
   return (
@@ -32,22 +38,64 @@ const Navbar = () => {
       <div className="navbar__middle">
         <ul>
           <Link href="/">
-            <li className={page === 1 ? "active__page hide__mobile" : "hide__mobile"} onClick={() => setCurrentPage(1)}>Home</li>
+            <li
+              className={
+                page === 1 ? "active__page hide__mobile" : "hide__mobile"
+              }
+              onClick={() => setCurrentPage(1)}
+            >
+              Home
+            </li>
           </Link>
           <Link href="/asd-renewables/about">
-            <li className={page === 2 ? "active__page hide__mobile" : "hide__mobile"} onClick={() => setCurrentPage(2)}>About Us</li>
+            <li
+              className={
+                page === 2 ? "active__page hide__mobile" : "hide__mobile"
+              }
+              onClick={() => setCurrentPage(2)}
+            >
+              About Us
+            </li>
           </Link>
           <Link href="/asd-renewables/about#solutions">
-            <li className={page === 3 ? "active__page hide__mobile" : "hide__mobile"} onClick={() => setCurrentPage(3)}>Solutions</li>
+            <li
+              className={
+                page === 3 ? "active__page hide__mobile" : "hide__mobile"
+              }
+              onClick={() => setCurrentPage(3)}
+            >
+              Solutions
+            </li>
           </Link>
           <Link href="/asd-renewables/services">
-            <li className={page === 4 ? "active__page hide__mobile" : "hide__mobile"} onClick={() => setCurrentPage(4)}>Services</li>
+            <li
+              className={
+                page === 4 ? "active__page hide__mobile" : "hide__mobile"
+              }
+              onClick={() => setCurrentPage(4)}
+            >
+              Services
+            </li>
           </Link>
           <Link href="/products">
-            <li className={page === 5 ? "active__page hide__mobile" : "hide__mobile"} onClick={() => setCurrentPage(5)}>Products</li>
+            <li
+              className={
+                page === 5 ? "active__page hide__mobile" : "hide__mobile"
+              }
+              onClick={() => setCurrentPage(5)}
+            >
+              Products
+            </li>
           </Link>
           <Link href="/asd-renewables/contact">
-            <li className={page === 6 ? "active__page hide__mobile" : "hide__mobile"} onClick={() => setCurrentPage(6)}>Contact Us</li>
+            <li
+              className={
+                page === 6 ? "active__page hide__mobile" : "hide__mobile"
+              }
+              onClick={() => setCurrentPage(6)}
+            >
+              Contact Us
+            </li>
           </Link>
         </ul>
         <div className="navbar__phone">
@@ -58,29 +106,88 @@ const Navbar = () => {
         </div>
       </div>
 
-
+      {/*Mobile View*/}
       <div className="navbar__right">
-        {/* <div className="hamburger">
-          <AiOutlineMenu className="hamburger__icon"/>
-          <ul>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Solutions</li>
-            <li>Services</li>
-            <li>Products</li>
-            <li>Contact Us</li>
-          </ul>
-        </div> */}
+        <div className="hamburger">
+          {showMobileMenu ? (
+            <GrClose className="hamburger__icon" onClick={setMenu} />
+          ) : (
+            <AiOutlineMenu className="hamburger__icon" onClick={setMenu} />
+          )}
 
+          {showMobileMenu && (
+            <ul>
+              <Link href="/">
+                <li onClick={() => setShowMobileMenu(false)}>Home</li>
+              </Link>
+              <Link href="/asd-renewables/about">
+                <li onClick={() => setShowMobileMenu(false)}>About Us</li>
+              </Link>
+              <Link href="/asd-renewables/about#solutions">
+                <li onClick={() => setShowMobileMenu(false)}>Solutions</li>
+              </Link>
+              <Link href="/asd-renewables/services">
+                <li onClick={() => setShowMobileMenu(false)}>Services</li>
+              </Link>
+              <Link href="/products">
+                <li onClick={() => setShowMobileMenu(false)}>Products</li>
+              </Link>
+              <Link href="/how-to-use">
+                <li onClick={() => setShowMobileMenu(false)}>
+                  How To Use Solar Energy
+                </li>
+              </Link>
+              <Link href="/how-solar-energy-works">
+                <li onClick={() => setShowMobileMenu(false)}>
+                  How Solar Energy Works
+                </li>
+              </Link>
+              <Link href="/faq">
+                <li onClick={() => setShowMobileMenu(false)}>
+                  Frequently Asked Questions
+                </li>
+              </Link>
 
-        {/* <div className="navbar__quote">
-          <span className="quote__icon">
-            <BsArrowRightCircleFill />
-          </span>
-          <Link href="/asd-renewables/quote">
-            <p>Get A Quote</p>
-          </Link>
-        </div> */}
+              <li onClick={() => setShowMobileMenu(false)}>Our Partners</li>
+              <Link href="/asd-renewables/contact">
+                <li onClick={() => setShowMobileMenu(false)}>Contact Us</li>
+              </Link>
+
+              <li>
+                <div className="navbar__phone">
+                  <span className="navbar__icon">
+                    <BsTelephoneFill />
+                  </span>
+                  <span className="phone__number">09084000487</span>
+                </div>
+              </li>
+              <li>
+                <div
+                  className="navbar__quote__mobile"
+                  onClick={() => setShowMobileMenu(false)}
+                >
+                  <span className="quote__icon">
+                    <BsArrowRightCircleFill />
+                  </span>
+                  <Link href="/asd-renewables/quote">
+                    <p>Get A Quote</p>
+                  </Link>
+                </div>
+              </li>
+            </ul>
+          )}
+        </div>
+
+        <div className="quote__cta__container hide__mobile">
+          <div className="navbar__quote">
+            <span className="quote__icon">
+              <BsArrowRightCircleFill />
+            </span>
+            <Link href="/asd-renewables/quote">
+              <p>Get A Quote</p>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
